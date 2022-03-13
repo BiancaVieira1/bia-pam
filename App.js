770e3967dc853d98edf28c2561a488f389faed96
unknown <biabr999@gmail.com>
2022-03-12 23:07:23 -0300

    proje calc

--- App.js
+++ App.js
@@ -1,11 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
+  const [valor1, setValor1] = useState();
+  const [valor2, setValor2] = useState();
+  const [resultado, setResultado] = useState(0);
+
+  function somar(){
+    setResultado(parseFloat(valor1) + parseFloat(valor2));
+  }
+  function subtrair(){
+    setResultado(parseFloat(valor1) - parseFloat(valor2));
+  }
+  function dividir(){
+    setResultado(parseFloat(valor1) / parseFloat(valor2));
+  }
+  function multiplicar(){
+    setResultado(parseFloat(valor1) * parseFloat(valor2));
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Calculadora</Text>  <p>
+
+      </p>
+        <Image
+        style={styles.logo}
+        source={{
+          uri: 'https://cdn-icons-png.flaticon.com/512/522/522506.png',
+        }}
+      />
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>Valor 1</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType="numeric"
+          value={valor1}
+          onChangeText={(texto)=>setValor1(texto)}
+        />
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>Valor 2</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType="numeric"
+          value={valor2}
+          onChangeText={(texto)=>setValor2(texto)}
+        />
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+          style={styles.botao1}
+          onPress={somar}
+        >
+            <Text style={styles.textoBotao1}>Somar</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+          style={styles.botao2}
+          onPress={subtrair}
+        >
+            <Text style={styles.textoBotao2}>Subtrair</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+          style={styles.botao3}
+          onPress={dividir}
+        >
+            <Text style={styles.textoBotao3}>Dividir</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+          style={styles.botao4}
+          onPress={multiplicar}
+        >
+            <Text style={styles.textoBotao4}>Multiplicar</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>Resultado: {resultado}</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +90,66 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#BA55D3',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textoBloco:{
+    fontSize:20,
+  },
+  texto:{
+    color:'#000',
+    fontSize:30
+  },
+  input:{
+    borderColor:'#000',
+    borderWidth:2,
+    fontSize:30,
+    width:'80%'
+  },
+  bloco:{
+    width:'100%',
+    alignItems:'center',
+    marginTop:30
+  },
+  botao1:{
+    backgroundColor:'#7B68EE',
+    width:'80%',
+    textAlign:'center'
+  },
+  botao2:{
+    backgroundColor:'#87CEFA',
+    width:'80%',
+    textAlign:'center'
+  },
+  botao3:{
+    backgroundColor:'#8A2BE2',
+    width:'80%',
+    textAlign:'center'
+  },
+  botao4:{
+    backgroundColor:'#9400D3',
+    width:'80%',
+    textAlign:'center'
+  },
+  textoBotao1:{
+    color:"#fff",
+    fontSize:30
+  },
+  textoBotao2:{
+    color:"#fff",
+    fontSize:30
+  },
+  textoBotao3:{
+    color:"#fff",
+    fontSize:30
+  },
+  textoBotao4:{
+    color:"#fff",
+    fontSize:30
+  }, 
+  logo:{
+    width:70,
+    height:70
+  }
 });
